90159906abdc72104408bd9dc726830f97034181
Avdeenko Ekaterina <avdeenqoeka@gmail.com>
2023-12-17 20:25:15 +0100

    line-intersection_AvdeenkoEkaterina

--- src/main/java/com/epam/rd/autotasks/intersection/Main.java
+++ src/main/java/com/epam/rd/autotasks/intersection/Main.java
@@ -3,18 +3,19 @@ package com.epam.rd.autotasks.intersection;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int k1 = scanner.nextInt();
-        int b1 = scanner.nextInt();
-        int k2 = scanner.nextInt();
-        int b2 = scanner.nextInt();
 
-        Line line1 = new Line(k1,b1);
-        Line line2 = new Line(k2,b2);
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int k1 = scanner.nextInt();
+    int b1 = scanner.nextInt();
+    int k2 = scanner.nextInt();
+    int b2 = scanner.nextInt();
 
-        System.out.println("result is " + line1.intersection(line2));
+    Line line1 = new Line(k1, b1);
+    Line line2 = new Line(k2, b2);
 
-    }
+    System.out.println("result is " + line1.intersection(line2));
+
+  }
 
 }
--- src/main/java/com/epam/rd/autotasks/intersection/Point.java
+++ src/main/java/com/epam/rd/autotasks/intersection/Point.java
@@ -1,16 +1,17 @@
 package com.epam.rd.autotasks.intersection;
 
 public class Point {
-    private final int x;
-    private final int y;
 
-    public Point(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
+  private final int x;
+  private final int y;
 
-    @Override
-    public String toString() {
-        return String.format("(%d;%d)", x, y);
-    }
+  public Point(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("(%d;%d)", x, y);
+  }
 }
